4033d4bbdb6adb88f964fd8ac975647a80b4c995
jakub-g <jakub.g.opensource@gmail.com>
2014-10-08 10:24:12 +0200

    WIP feat(syntax) Use HTML attributes for <template> parameters
    Conflicts:
    	docs/playground/splitter.hsp
    	docs/samples/logs/logs.hsp
    	test/compiler/errsamples/element6.txt
    	test/compiler/errsamples/element7.txt
    	test/gestures/doubleTap.spec.hsp
    	test/gestures/drag.spec.hsp
    	test/gestures/longPress.spec.hsp
    	test/gestures/pinch.spec.hsp
    	test/gestures/singleTap.spec.hsp
    	test/gestures/swipe.spec.hsp
    	test/gestures/tap.spec.hsp
    	test/rt/cptattelements3.spec.hsp
    	test/rt/cptattelements4.spec.hsp
    	test/rt/evthandler.spec.hsp

--- gruntfile.js
+++ gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
       test: {
         options: {
           reporter: 'spec',
-          require: 'build/blanket' // comment this line out to disable coverage when debugging mocha tests
+//          require: 'build/blanket' // comment this line out to disable coverage when debugging mocha tests
         },
         src: [
           'test/compiler/**/*.js',
